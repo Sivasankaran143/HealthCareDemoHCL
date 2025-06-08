7fb2736d2cda13f81ac94166b60ebbdc2c73ff9a
Sakthisivam <manimari.siva143@gmail.com>
2025-06-08 14:26:35 +0530

    Changes linq to lamdaexpression GetAllStaffDetails

---
+++ Repository/HealthCareRepostory.cs
@@ -0,0 +1,24 @@
+﻿using HealthCareDemoDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareDemoDataAccess
+{
+    public class HealthCareRepostory
+    {
+        private HealthCareContext _context;
+        public HealthCareRepostory(HealthCareContext context)
+        {
+            _context = context;
+        }
+        public List<staff> GetAllStaffDetails()
+        {
+            //var staffList = (from staffListDb in _context.staff select staffListDb).ToList();
+            var staffList = _context.staff.ToList();
+            return staffList;
+        }
+    }
+}
